0c9956a54c620e35ba3fbe4b5abb71b709039aa3
Neelzee <nilsien2001@gmail.com>
2023-12-19 13:46:53 +0100

    improved offline sok saving

--- src/app/offline_app.rs
+++ src/app/offline_app.rs
@@ -3,7 +3,7 @@ use std::{time::Instant, fs::{self, File}, io::Read};
 use itertools::Itertools;
 use scraper::Html;
 
-use crate::{error::ArchiveError, modules::webpage::{Link, Webpage}, parser::{wp::get_sok_collection, medium}, xl::save_sok, app::main_app::{checkmark_sok, write_log}, utils::constants::ROOT_URL};
+use crate::{error::ArchiveError, modules::{webpage::{Link, Webpage}, sok::{SokCollection, Merknad}}, parser::{wp::{get_sok_collection, get_kilde, get_metode}, medium, get_merknad}, xl::save_sok, app::main_app::{checkmark_sok, write_log}, utils::constants::ROOT_URL};
 
 use std::path::Path;
 
@@ -64,49 +64,51 @@ pub async fn get_soks_offline() -> Result<(), ArchiveError> {
         
         let time_start = Instant::now();
 
-        if let Some(com) = wp.get_forms().ok() {
-            let count = com.combinations().collect::<Vec<_>>().len();
-            if count <= 30 {
-                println!("Sok: {}", wp.get_id());
-            } else {
-                println!("Sok: {}", wp.get_id());
-                println!("Form Combo: {:?}", count);
-                continue;
-            }
-        }
         let link = wp.get_url();
-        match get_sok_collection(wp).await {
-            Ok((sokc, mut errs)) => {
-                sok_log.append(&mut errs);
-                let path = format!("arkiv\\{}", medium.clone());
+        let path = format!("arkiv\\{}", medium.clone());
 
-                let r = fs::create_dir_all(path.clone());
-                if r.is_err() {
-                    println!("Could not create path: {}, got error: {}", path.clone(), r.unwrap_err());
-                }
+        let mut sokc = SokCollection::new(id, medium);
 
-                let time_end = Instant::now();
-
-                // Failed
-                if sokc.sok.len() == 0 {
-                    println!("Sok: {}, had 0 tables.", &sokc.id);
-                    sok_log.push(ArchiveError::FailedParsing(sokc.id.clone(), link));
-                    continue;
-                }
-
-                match save_sok(sokc, &path) {
-                    Ok(_) => {
-                        checkmark_sok(&id);
-                        println!("Saved sok: {}, Took {}s", &id, (time_end - time_start).as_secs());
-                    },
-                    Err(e) => {
-                        println!("Failed saving sok: {}, With Error: {}, Took {}s", &id, &e, (time_end - time_start).as_secs());
-                        sok_log.push(e);
-                    }, 
-                }
-            },
-            Err(e) => sok_log.push(e)
+        let r = fs::create_dir_all(path.clone());
+        if r.is_err() {
+            println!("Could not create path: {}, got error: {}", path.clone(), r.unwrap_err());
         }
+
+        let time_end = Instant::now();
+
+        sokc.add_sok(wp.get_sok()?);
+
+        // Kilde
+        for k in get_kilde(&wp).await? {
+            sokc.add_kilde(k.into());
+        }
+
+        // Metode
+        for m in get_metode(&wp).await? {
+            sokc.add_metode(m.into());
+        }
+
+        // Merknad
+        sokc.add_merknad(Merknad { title: "Merknad".to_string(), content: wp.get_merknad()? });
+
+        // Failed
+        if sokc.sok.len() == 0 {
+            println!("Sok: {}, had 0 tables.", &sokc.id);
+            sok_log.push(ArchiveError::FailedParsing(sokc.id.clone(), link));
+            continue;
+        }
+
+        match save_sok(sokc, &path) {
+            Ok(_) => {
+                checkmark_sok(&id);
+                println!("Saved sok: {}, Took {}s", &id, (time_end - time_start).as_secs());
+            },
+            Err(e) => {
+                println!("Failed saving sok: {}, With Error: {}, Took {}s", &id, &e, (time_end - time_start).as_secs());
+                sok_log.push(e);
+            }, 
+        }
+
         if sok_log.len() != 0 {
             println!("{} error(s) found.", sok_log.len());
             let r = write_log(sok_log.clone().into_iter().map(|e| e.to_string()).collect_vec(), id);
